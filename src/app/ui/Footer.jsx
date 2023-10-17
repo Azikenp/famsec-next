95b49db425f1a5f0921203a4386979841d1b7a84
Azikenp <azikenp26@gmail.com>
2023-10-17 03:15:34 +0100

    more

--- src/app/ui/Footer.jsx
+++ src/app/ui/Footer.jsx
@@ -7,7 +7,7 @@ import { HiPhone, HiMiniGlobeAmericas, HiMiniHeart } from "react-icons/hi2";
 
 const footerContent = {
   about: {
-    logo: "/image/logo/svg",
+    logo: "/images/logo.svg",
     description:
       "From heartwarming testimonials to glowing reviews, these voices reflect the trust and confidence our customers place in us. Let their words speak volumes about the extraordinary experiences that await you with",
     cta: {
@@ -88,7 +88,49 @@ const Footer = () => {
     <footer role="contentinfo" className="py-20 bg-white">
       <div className="container px-4 mx-auto">
         <div className="block lg:flex gap-20 mb-10 pb-10">
-            <div className="w-full lg:w-4/12 mb-10 lg:mb-0"></div>
+          <div className="w-full lg:w-4/12 mb-10 lg:mb-0">
+            <Link href="/" className="mb-4 inline-block">
+              <Image
+                src={footerContent.about.logo}
+                width={157}
+                height={30}
+                alt="image"
+              />
+            </Link>
+            <p className="leading-relaxed mb-7">
+              {footerContent.about.description}
+            </p>
+            <p>
+              <Link
+                href={footerContent.about.cta.href}
+                className="flex space-x-2 outline-none items-center font-semibold text-primary group"
+              >
+                <span>{footerContent.about.cta}</span>
+                <span className="w-6 h-6 rounded-full bg-primary text-white inline-flex items-center justify-center">
+                  <BiChevronRight className="w-6 h-6 rounded-full bg-primary text-white inline-flex items-center justify-center duration-300 transition-all ease-in-out group-hover:bg-secondary" />
+                </span>
+              </Link>
+            </p>
+          </div>
+
+          <div className="w-full lg:w-4/12 mb-10 lg:mb-0">
+            <div className="grid grid-cols-2 gap-10">
+              <ul className="p-0 m-0">
+                {footerContent.footerLinks.map((link, idx) => (
+                  <li className="mb-3" key={idx}>
+                    <Link className="group flex items-center duration-300 transition-all ease-in-out hover:text-primary">
+                      <span>{link.label}</span>
+                      <span className="left-2 relative duration-300 transition-all ease-in-out opacity-0 group-hover:opacity-100 group-hover:left-3">
+                        <BiChevronRight className="text-xl" />
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="w-4/12"></div>
         </div>
       </div>
     </footer>
